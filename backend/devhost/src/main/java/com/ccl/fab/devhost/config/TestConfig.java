fe39a7f3d9f5977b3fae8619acf323f7ac1e77e5
Fabiano <fabianofac_@hotmail.com>
2021-11-17 17:06:52 -0300

    dependencies to deploy and configs

--- backend/devhost/src/main/java/com/ccl/fab/devhost/config/TestConfig.java
+++ backend/devhost/src/main/java/com/ccl/fab/devhost/config/TestConfig.java
@@ -36,20 +36,20 @@ public class TestConfig implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		User user1 = new User(null, "Maria", "Data Scientist", "Degree"," http://foto", WorkSituation.FREELANCER, bc.encode("123"));
-		User user2 = new User(null, "João", "Cloud", "Degree", "http://foto2", WorkSituation.EMPLOYED, bc.encode("123"));
-		User user3 = new User(null, "Pedro", "Design", "Post-Degree", "http://foto2", WorkSituation.UNEMPLOYED, bc.encode("123"));
-		User user4 = new User(null, "Fabs", "Java Dev", "Degree"," http://foto6", WorkSituation.FREELANCER, bc.encode("123"));
+		User user1 = new User(null, "Maria Fernada", "Data Scientist", "Degree"," http://foto", WorkSituation.FREELANCER, bc.encode("123"));
+		User user2 = new User(null, "João Batista", "Cloud", "Degree", "http://foto2", WorkSituation.EMPLOYED, bc.encode("123"));
+		User user3 = new User(null, "Pedro Paulo", "Design", "Post-Degree", "http://foto2", WorkSituation.UNEMPLOYED, bc.encode("123"));
+		User user4 = new User(null, "Fabiano Carregas", "Java", "Degree"," http://foto6", WorkSituation.FREELANCER, bc.encode("123"));
 		
 		user4.addProfilee(Profilee.ADMIN);
 		
 		userRepository.saveAll(Arrays.asList(user1, user2, user3, user4));
 		
-		Contact contact1= new Contact(null, "maria@Gmail.com", "11-99999999", "maria@github", "maria@linkedin",user1);
-		Contact contact2= new Contact(null, "joao@Gmail.com", "11-88889999", "joao@github", "joao@linkedin", user2);	
-		Contact contact3= new Contact(null, "pedro@Gmail.com", "11-77779999", "pedro@github", "pedro@linkedin", user3);
+		Contact contact1= new Contact(null, "maria@Gmail.com", "11-99999999", "maria@github.com", "maria@linkedin.com",user1);
+		Contact contact2= new Contact(null, "joao@Gmail.com", "11-88889999", "joao@github.com", "joao@linkedin.com", user2);	
+		Contact contact3= new Contact(null, "pedro@Gmail.com", "11-77779999", "pedro@github.com", "pedro@linkedin.com", user3);
 		Contact contact4= new Contact(null, "pedro2@Gmail.com", "11-77229999", null, null, user3);
-		Contact contact5= new Contact(null, "fabs@Gmail.com", "11-74479999", "fabs@github", "fabs@linkedin", user4);
+		Contact contact5= new Contact(null, "fabs@Gmail.com", "11-74479999", "fabs@github.com", "fabs@linkedin.com", user4);
 		
 		user1.getContacts().add(contact1);
 		user2.getContacts().add(contact2);
@@ -58,12 +58,12 @@ public class TestConfig implements CommandLineRunner{
 
 		contactRepository.saveAll(Arrays.asList(contact1, contact2, contact3, contact4, contact5));
 	
-		Project project1 = new Project(null, "Aplicativo de pressão de combustivel", "www.nasa.com", "github@nasa", user1);
-		Project project2 = new Project(null, "Cadastro de Astronautas", "www.nasa2.com", "github@nasa2", user1);
-		Project project3 = new Project(null, "Controle de carregamento da bateria", "www.projects.com", "github@project", user2);
-		Project project4 = new Project(null, "Super mercado", "www.mercadinho.com", "github@sm", user2);
-		Project project5 = new Project(null, "E-commerce", "www.eshop.com", "github@es", user3);
-		Project project6 = new Project(null, "Dev-Host", "www.webpage.com", "github@fabs", user4);
+		Project project1 = new Project(null, "Aplicativo de pressão de combustivel", "www.nasa.com", "github@nasa.com", user1);
+		Project project2 = new Project(null, "Cadastro de Astronautas", "www.nasa2.com", "github@nasa2.com", user1);
+		Project project3 = new Project(null, "Controle de carregamento da bateria", "www.projects.com", "github@project.com", user2);
+		Project project4 = new Project(null, "Super mercado", "www.mercadinho.com", "github@sm.com", user2);
+		Project project5 = new Project(null, "E-commerce", "www.eshop.com", "github@es.com", user3);
+		Project project6 = new Project(null, "Dev-Host", "www.webpage.com", "github@fabs.com", user4);
 		
 		projectRepository.saveAll(Arrays.asList(project1, project2, project3, project4, project5, project6));
 	
